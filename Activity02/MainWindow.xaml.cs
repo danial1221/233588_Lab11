c9470f4b30cb72c1d648e3851e8fc8d7639e75f3
Danial <danialmujahid98@gmail.com>
2024-12-10 14:28:06 +0500

    Add files via upload

---
+++ Activity02/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace WPFCheckBoxControl
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            if (sender == checkBox1)
+                textBox1.Text = "Two States Checked";
+            else if (sender == checkBox2)
+                textBox2.Text = "Three States Checked";
+        }
+
+        private void HandleUnchecked(object sender, RoutedEventArgs e)
+        {
+            if (sender == checkBox1)
+                textBox1.Text = "Two States Unchecked";
+            else if (sender == checkBox2)
+                textBox2.Text = "Three States Unchecked";
+        }
+
+        private void HandleThirdState(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = "Three States Indeterminate";
+        }
+    }
+}
